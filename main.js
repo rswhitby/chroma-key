23d7047844ecddaef4a8fb0bb66fc9573d1cd1e5
rswhitby <rswhitby@gmail.com>
2025-07-28 10:53:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const streams = {
 const hlsStreams = {
   red:    "https://stream.mux.com/qbmLFMnR6yLLVQNDWxTd00xtWe00Prw02009Z01brhf4U8QE.m3u8",
   green:  "https://stream.mux.com/3szftA95p4XgdnVx7kkj7OVkw28Ya9xj3Ps4eJsGziE.m3u8",
-  blue:   "https://stream.mux.com/yjwOhbdvbQhd2dejZsrOrS2F00At01zpSow3BJYchU7vQ.m3u8",
+  blue:   "https://stream.mux.com/00X02iElluPmjN2FXO4JOnKW2weV3EhP018PDnfeMsKXCE.m3u8",
   yellow: "https://stream.mux.com/g00DWGCoz02YMASwERCbRvKDVbdKehBsP8sL02i4KZIGBY.m3u8",
 };
 
