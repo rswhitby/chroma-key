0f4f6f998a7c0485689baf3ce7e9439517f59094
rswhitby <rswhitby@gmail.com>
2025-07-28 10:59:52 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const hlsStreams = {
   red:    "https://stream.mux.com/qbmLFMnR6yLLVQNDWxTd00xtWe00Prw02009Z01brhf4U8QE.m3u8",
   green:  "https://stream.mux.com/3szftA95p4XgdnVx7kkj7OVkw28Ya9xj3Ps4eJsGziE.m3u8",
   blue:   "https://stream.mux.com/00X02iElluPmjN2FXO4JOnKW2weV3EhP018PDnfeMsKXCE.m3u8",
-  yellow: "https://stream.mux.com/g00DWGCoz02YMASwERCbRvKDVbdKehBsP8sL02i4KZIGBY.m3u8",
+  yellow: "https://stream.mux.com/ktMc1XWr2HpKRsd7S5BtVsXK3pEF4zL01LPSukXvaD01k.m3u8",
 };
 
 // 4. Initialize Hls.js on each overlay
