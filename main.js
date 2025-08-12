47f4dd1c830429c97beb9f46c100af23038f13f9
rswhitby <rswhitby@gmail.com>
2025-08-12 06:43:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ const streams = {
 // 3. HLS URLs
 const hlsStreams = {
   red:    "https://stream1.ovationav.com/red.m3u8",
-  green:  "https://stream1.ovationav.com/green.m3u8",
+  green:  "https://stream.mux.com/hwVrqOk3HFBHKJu01DEYNYu7Pre5DwczAnbOCvBY6oxo.m3u8",
   blue:   "https://stream1.ovationav.com/blue.m3u8",
   yellow: "https://stream1.ovationav.com/yellow.m3u8",
 };
