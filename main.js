04f6ca3565749252cadfea1c93df0076bbe433a2
rswhitby <rswhitby@gmail.com>
2025-08-14 08:40:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const streams = {
   yellow: document.getElementById("video-yellow"),
 };
 
+/*
 // ----- your HLS URLs -----
 const hlsStreams = {
   red: "https://stream.mux.com/KtpDUWnzBLkuBy6x2LNsnfGfMuL8n02EY3vwd8ySCoAQ.m3u8",
@@ -23,7 +24,15 @@ const hlsStreams = {
   blue: "https://stream.mux.com/rMcdMotmURzskzSV5ZI4w8x8LkIuXyalfGTWLb1DqvI.m3u8",
   yellow: "https://stream.mux.com/qAJ01jpHaXsLFRt7NDjfOboj3pOiZ5h5SHk6pKNt51tI.m3u8",
 };
+*/
 
+// ----- your HLS URLs -----
+const hlsStreams = {
+  red: "https://stream1.ovationav.com/red.m3u8",
+  green: "https://stream1.ovationav.com/green.m3u8",
+  blue: "https://stream1.ovationav.com/blue.m3u8",
+  yellow: "https://stream1.ovationav.com/yellow.m3u8",
+};
 
 // ----- init HLS on each overlay -----
 Object.entries(streams).forEach(([color, video]) => {
